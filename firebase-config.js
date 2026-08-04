1187187d01328d494aec836005dfd37fbeb012c1
Laaaauree <laureenogueira@gmail.com>
2026-08-04 11:02:48 -0300

    subindo projeto

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmn0YA71iVXgyvb8627D-rv1ZVtkCHLF4",
-  authDomain: "agenda-dpips.firebaseapp.com",
-  projectId: "agenda-dpips",
-  storageBucket: "agenda-dpips.firebasestorage.app",
-  messagingSenderId: "10526411332",
-  appId: "1:10526411332:web:77ef3682a408448bf18a11"
+  apiKey: "AIzaSyBfb6qslnV0Hr015QACJFZpyj8eTVYrfII",
+  authDomain: "dpips-f9b0f.firebaseapp.com",
+  projectId: "dpips-f9b0f",
+  storageBucket: "dpips-f9b0f.firebasestorage.app",
+  messagingSenderId: "578993170008",
+  appId: "1:578993170008:web:bbfcc16f96d18abfb24c2d"
 };
 
 // Inicializar Firebase
